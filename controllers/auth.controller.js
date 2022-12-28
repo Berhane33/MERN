72a634f010d759a19485d0bd511cd016a7696363
Jacob Messou <jacob.messou@gmail.com>
2022-12-28 00:18:04 -0800

    second day of work

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,13 +1,36 @@
-const userModel = require('../models/user.model');
+const UserModel = require("../models/user.model");
+const jwt = require("jsonwebtoken");
+
+const maxAge = 3 * 24 * 60 * 1000;
+
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.TOKEN_SECRET, {
+    epiresIn: maxAge,
+  });
+};
 
 module.exports.signUp = async (req, res) => {
-    const {pseudo, email, password} = req.body
-    
-    try {
-        const user = await userModel.create({pseudo, email, password});
-        res.status(201).json({user: user._id});
-    }
-    catch(err) {
-        res.status(200).send({err})
-    }
-}
+  const { pseudo, email, password } = req.body;
+
+  try {
+    const user = await UserModel.create({ pseudo, email, password });
+    res.status(201).json({ user: user._id });
+  } catch (err) {
+    res.status(200).send({ err });
+  }
+};
+
+module.exports.signIn = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await UserModel.login(email, password);
+    const token = createToken(user._id);
+    res.cookie("jwt", token, { httpOnly: true, maxAge });
+    res.status(200).json({ user: user._id });
+  } catch (err) {
+    res.status(200).json(err);
+  }
+};
+
+module.exports.logout = (req, res) => {};
--- models/user.model.js
+++ models/user.model.js
@@ -1,6 +1,5 @@
-const mongoose = require('mongoose');
-const { isEmail } = require('validator');
-
+const mongoose = require("mongoose");
+const { isEmail } = require("validator");
 
 const userSchema = new mongoose.Schema(
   {
@@ -10,7 +9,7 @@ const userSchema = new mongoose.Schema(
       minLength: 3,
       maxLength: 55,
       unique: true,
-      trim: true
+      trim: true,
     },
     email: {
       type: String,
@@ -24,36 +23,48 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
       max: 1024,
-      minlength: 6
+      minlength: 6,
     },
     picture: {
       type: String,
-      default: "./uploads/profil/random-user.png"
+      default: "./uploads/profil/random-user.png",
     },
-    bio :{
+    bio: {
       type: String,
       max: 1024,
     },
     followers: {
-      type: [String]
+      type: [String],
     },
     following: {
-      type: [String]
+      type: [String],
     },
     likes: {
-      type: [String]
-    }
+      type: [String],
+    },
   },
   {
     timestamps: true,
   }
 );
 
-userSchema.pre("save", async function(next) {
-    const salt = await bcrypt.genSalt();
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
+userSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
+
+userSchema.statics.login = async function (email, password) {
+  const user = await this.findOne({ email });
+  if (user) {
+    const auth = await bcrypt.compare(password, user.password);
+    if (auth) {
+      return user;
+    }
+    throw Error("Incorrect password");
+  }
+  throw Error("Incorrect email");
+};
 
 const UserModel = mongoose.model("user", userSchema);
 
